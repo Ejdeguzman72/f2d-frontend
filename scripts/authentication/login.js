2a1b7392cadcc11078f937e5388f1d8809adb35d
Ejdeguzman72 <ejdeguzman72@gmail.com>
2024-11-04 00:11:57 -0500

    more changs

---
+++ scripts/authentication/login.js
@@ -0,0 +1,39 @@
+async function login() {
+    const username = document.getElementById('username').value;
+    console.log(username)
+    if (username == '') {
+        alert('Username must be filled')
+    }
+    const password = document.getElementById('password').value;
+    if (password == '') {
+        alert('Password must be filled');
+    }
+
+    try {
+        // Make a request to your authentication API endpoint
+        const response = await fetch('http://192.168.1.54:8080/auth/authenticate', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, password }),
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+
+            // Store the JWT token in localStorage
+            localStorage.setItem('ejjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj', data.accessToken);
+            
+            if (data.accessToken) {
+                // Redirect to the home page (you can replace this with your desired URL)
+                window.location.href = 'home-page.html';
+            }
+        } else {
+            // Handle authentication failure
+            console.error('Authentication failed');
+        }
+    } catch (error) {
+        console.error('Error during login:', error.message);
+    }
+}
